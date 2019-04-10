23c11922e37f747c7739ee2c530212a61231ed63
GlobeCyber <Github@GlobeCyber.com>
2019-04-10 21:24:18 +0430

    Initial commit

---
+++ libs/chrome.py
@@ -0,0 +1,77 @@
+import sqlite3
+import os
+import datetime
+from libs.general import general
+
+class chrome(general):
+    config = {
+        'hisotry_database_name' : 'History',
+        'platform_profile_path' : {
+            'darwin' : 'Library/Application Support/Google/Chrome',
+        }
+    }
+
+    def __init__(self):
+        general.__init__(self)
+        self.profiles_path = os.path.join(self.user_home, self.config['platform_profile_path'][self.platform_name])
+
+    # Private Methods
+    def _convert_date_from_webkit(self, webkit_timestamp):
+        epoch_start = datetime.datetime(1601,1,1)
+        delta = datetime.timedelta(microseconds=int(webkit_timestamp))
+        return epoch_start + delta
+
+    def downloads(self, profile_path):
+        # TODO : Filtering By : ['fileextension', 'date', 'daterange', 'domain']
+        try:
+            connection = sqlite3.connect(os.path.join(profile_path, self.config['hisotry_database_name']))
+            db_cursor = connection.cursor()
+            downloaded_files = db_cursor.execute("SELECT tab_url, target_path, start_time, total_bytes, state FROM downloads;").fetchall()
+            downloads = []
+            for download_item in downloaded_files:
+
+                is_fully_download = False
+                if download_item[4] == 1:
+                    is_fully_download = True
+
+                downloads.append({
+                    'url' : download_item[0],
+                    'saved_in' : str(download_item[1]),
+                    'start_downloading_at' : str(self._convert_date_from_webkit(download_item[2])),
+                    'size' : download_item[3],
+                    'is_fully_download' : is_fully_download
+                })
+
+            db_cursor.close()
+
+            return downloads   
+        except Exception as error:
+            print('Error : ' + str(error))
+            exit()
+
+    def history(self, profile_path):
+        try:
+            connection = sqlite3.connect(os.path.join(profile_path, self.config['hisotry_database_name']))
+            db_cursor = connection.cursor()
+            db_cursor.execute("SELECT url, visit_count, last_visit_time FROM urls ORDER BY visit_count;")
+            urls = db_cursor.fetchall()
+            parsed_histories = []
+            for url in urls:
+                parsed_histories.append({
+                    'url' : url[0],
+                    'count' : str(url[1]),
+                    'last_visit' : str(self._convert_date_from_webkit(url[2]))
+                })
+            db_cursor.close()
+            return parsed_histories        
+        except Exception as error:
+            print('Error : ' + str(error))
+            exit()
+
+    def get_profiles(self):
+        profiles = []
+        if os.path.isdir(self.profiles_path):
+            for profile in os.listdir(self.profiles_path):
+                if(profile == 'Default' or 'Profile' in profile):
+                    profiles.append({'path' : self.profiles_path + '/' + profile, 'name': profile, 'browser': self.__class__.__name__})
+        return profiles
---
+++ libs/firefox.py
@@ -0,0 +1,84 @@
+import sqlite3
+import os
+import json
+from datetime import datetime
+from libs.general import general
+
+class firefox(general):
+
+    config = {
+        'hisotry_database_name' : 'places.sqlite',
+        'platform_profile_path' : {
+            'windows' : 'AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\',
+            'darwin' : 'Library/Application Support/Firefox/Profiles',
+            'linux' : '.mozilla/firefox/Profiles/'
+        }
+    }
+
+    def __init__(self):
+        general.__init__(self)
+        self.profiles_path = os.path.join(self.user_home, self.config['platform_profile_path'][self.platform_name])
+
+    def history(self, profile_path, filters=None):
+        query_filters = ""
+        if filters:
+            query_filters = " WHERE "
+            if filters.get('domain'):
+                query_filters += "url LIKE 'http'"
+
+        query = "SELECT url, visit_count, datetime(last_visit_date/1000000,'unixepoch') FROM moz_places"+query_filters+' ORDER BY visit_count;'
+        print(query)
+        # try:
+        connection = sqlite3.connect(os.path.join(profile_path, self.config['hisotry_database_name']))
+        db_cursor = connection.cursor()
+        db_cursor.execute(query)
+        urls = db_cursor.fetchall()
+        parsed_histories = []
+        for url in urls:
+            parsed_histories.append({
+                'url' : url[0],
+                'count' : str(url[1]),
+                'last_visit' : str(url[2])
+            })
+        db_cursor.close()
+        return parsed_histories        
+        # except Exception as error:
+        #     print('Error : ' + str(error))
+        #     exit()
+
+    def downloads(self, profile_path):
+        try:
+            connection = sqlite3.connect(os.path.join(profile_path, self.config['hisotry_database_name']))
+            db_cursor = connection.cursor()
+            downloaded_files = db_cursor.execute("SELECT places.url, basic_info.content, basic_info.dateAdded, extended_info.content FROM moz_annos AS basic_info JOIN moz_annos AS extended_info ON basic_info.place_id=extended_info.place_id JOIN moz_places as places ON basic_info.place_id=places.id WHERE basic_info.anno_attribute_id='4' AND extended_info.anno_attribute_id='6'").fetchall()
+        
+            downloads = []
+            for url in downloaded_files:
+                download_metadata = json.loads(url[3])
+
+                is_fully_download = False
+                if download_metadata['state'] == 1:
+                    is_fully_download = True
+                
+                filesize = download_metadata.get('fileSize')
+                downloads.append({
+                    'url' : url[0],
+                    'saved_in' : str(url[1][7:]),
+                    'start_downloading_at' : datetime.fromtimestamp(float(url[2])/1000000.0).strftime('%Y-%m-%d %H:%M:%S'),
+                    'filesize' : filesize,
+                    'is_fully_download' : is_fully_download
+                })
+
+            db_cursor.close()
+            return downloads
+
+        except Exception as error:
+            print('Error : ' + str(error))
+            exit()
+    
+    def get_profiles(self):
+        profiles = []
+        if os.path.isdir(self.profiles_path):
+            for profile in os.listdir(self.profiles_path):
+                profiles.append({'path' : self.profiles_path + '/' + profile, 'name': profile, 'browser': self.__class__.__name__})
+        return profiles
